2cf408ede0287f4924d3b92afae4a860b6e7985e
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2022-11-21 19:48:30 +0000

    update run_rocoto.sh

--- run_rocoto.sh
+++ run_rocoto.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+# Change directory to where this script is located
+
+fullpath=$( readlink -f "${BASH_SOURCE[0]}" )
+path=$( dirname "${fullpath}" )
+cd $path
+
+source /etc/profile.d/modules.sh
 module load rocoto
 rocotorun -w nature_wflow.xml -d nature_wflow.db
