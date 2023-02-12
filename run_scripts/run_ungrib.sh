e979b3b62946f55041e36cdb7169feac1e4afcd3
smurd <shawn.s.murdzek@noaa.gov>
2023-02-12 15:46:15 +0000

    fix bug in run_ungrib.sh when starting a new day

--- run_scripts/run_ungrib.sh
+++ run_scripts/run_ungrib.sh
@@ -7,6 +7,9 @@ cd ${WORKDIR}
 cp ${WPSDIR}/link_grib.csh .
 cp ${WPSDIR}/ungrib.exe .
 
+# Determine hour of startdate
+hr=${startdate:11:2}
+
 if [ -e ${NAMELISTDIR}/namelist.wps.HRRR ]; then
 
   # Only have HRRR data for ICs. Give priority to HRRR data in ungrib
@@ -21,23 +24,39 @@ if [ -e ${NAMELISTDIR}/namelist.wps.HRRR ]; then
   rm GRIB*
 
   # Only grab RAP data for the day the cycle is starting (RAPDIR1) and
-  # the next day (RAPDIR2) otherwise ungrib takes too long to run
+  # the next day (RAPDIR2) otherwise ungrib takes too long to run.
+  # If this is the first cycle on this particular day, also grab the RAP data
+  # from the previous day (RAPDIR3).
   cp ${NAMELISTDIR}/namelist.wps.RAP .
   sed -i "s|START|${startdate}|g" namelist.wps.RAP
   sed -i "s|END|${lastdate}|g" namelist.wps.RAP
   ln -sf namelist.wps.RAP namelist.wps
-  ./link_grib.csh ${RAPDIR1}/* ${RAPDIR2}/*
+  if [[ $hr -eq '00' && -d ${RAPDIR3} ]]; then
+    echo 'beginning of a new day'
+    ./link_grib.csh ${RAPDIR1}/* ${RAPDIR3}/*
+  else
+    ./link_grib.csh ${RAPDIR1}/* ${RAPDIR2}/*
+  fi
   ln -sf ${STATICDIR}/WPS/Vtable.raphrrr.SSM Vtable
   ./ungrib.exe
   e=$?
 
 elif [ -e ${NAMELISTDIR}/namelist.wps.RAP ]; then
 
+  # Only grab RAP data for the day the cycle is starting (RAPDIR1) and
+  # the next day (RAPDIR2) otherwise ungrib takes too long to run.
+  # If this is the first cycle on this particular day, also grab the RAP data
+  # from the previous day (RAPDIR3).
   cp ${NAMELISTDIR}/namelist.wps.RAP .
   sed -i "s|START|${startdate}|g" namelist.wps.RAP
   sed -i "s|END|${lastdate}|g" namelist.wps.RAP
   ln -sf namelist.wps.RAP namelist.wps
-  ./link_grib.csh ${RAPDIR1}/* ${RAPDIR2}/*
+  if [[ $hr -eq '00' && -d ${RAPDIR3} ]]; then
+    echo 'beginning of a new day'
+    ./link_grib.csh ${RAPDIR1}/* ${RAPDIR3}/*
+  else
+    ./link_grib.csh ${RAPDIR1}/* ${RAPDIR2}/*
+  fi
   ln -sf ${STATICDIR}/WPS/Vtable.raphrrr.SSM Vtable
   ./ungrib.exe
   e=$?
