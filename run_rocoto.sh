f372ff330a369d8ac7fd3854b367da276060dd06
Shawn Murdzek <shawn.s.murdzek@noaa.gov>
2023-03-22 16:11:49 -0500

    update run_rocoto.sh to work on Orion and Hercules

--- run_rocoto.sh
+++ run_rocoto.sh
@@ -5,9 +5,12 @@ fullpath=$( readlink -f "${BASH_SOURCE[0]}" )
 path=$( dirname "${fullpath}" )
 cd $path
 
-# Add ability to load modules and use slurm
+# Add ability to load modules and use slurm (Hera + Jet)
 source /etc/profile.d/modules.sh
 source /etc/profile.d/slurm.sh
+
+# Add ability to load modules and use slurm (Orion + Hercules)
+source /etc/profile
 
 module load rocoto
 rocotorun -w nature_wflow.xml -d nature_wflow.db
